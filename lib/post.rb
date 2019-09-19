e6d18ee40c620e61532310e7ff54a1bc4b77cf02
Marco-FI-SEI <mmc90@hotmail.co.uk>
2019-09-19 09:41:11 +0100

    Done.

---
+++ lib/post.rb
@@ -0,0 +1,21 @@
+class Post
+    attr_accessor :author, :title
+    @@all = []
+
+    def initialize(title)
+        @title = title
+        save
+    end
+
+    def save
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def author_name
+        return self.author.nil? ? nil : self.author.name
+    end
+end
